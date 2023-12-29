52454edafecca0861cceb0a0255e1e19adbc5465
Marshular's Development <78507312+marshular@users.noreply.github.com>
2023-12-28 21:35:09 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,16 @@
-author "5M-CodeX | TheStoicBear"
-description "codex-opticom"
-version "2.0.0"
-
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
-client_script "source/client.lua"
-server_script "source/server.lua"
+author "5M-CodeX | TheStoicBear"
+description "codex-opticom"
+version "2.0.0"
+
+shared_scripts {
+  "@ox_lib/init.lua",
+  "config_shared.lua"
+}
+
+client_script "source/client/main.lua"
+
+server_script "source/server/main.lua"
