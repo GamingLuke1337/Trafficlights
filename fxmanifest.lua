79108ccb5ed4b3e7166582344b1af7b3b6b2e199
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-02-25 17:05:45 +0100

    added Github link

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ lua54 'yes'
 
 author 'TheStoicBear - Reworked by Gamingluke1337'
 description 'Trafficlight script for FiveM'
+repository 'https://github.com/GamingLuke1337/Trafficlights'
 version '1.1.0'
 
 client_script 'client/main.lua'
